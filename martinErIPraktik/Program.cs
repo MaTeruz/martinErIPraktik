d60061d6eb8cdcf4b4d6d26f37a01e7a8cf7c19f
stuzb <stuzb@LAPTOP-7UV7VFFK>
2021-08-31 09:17:47 +0200

    Add project files.

---
+++ martinErIPraktik/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace martinErIPraktik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            square firkant = new square(0.5578f);
+            square garry = new square();
+
+            
+
+            Console.WriteLine(firkant.Side);
+            Console.WriteLine(firkant.Perimitor());
+            Console.WriteLine(garry.Side);
+            Console.WriteLine(garry.Perimitor());
+            Console.WriteLine(firkant.Area());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ martinErIPraktik/bobTheBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace martinErIPraktik
+{
+    public class square
+    {
+        private float side;
+        public square(float side) {this.side = side;}
+        public square() { }
+        public float Side 
+        {
+            get { return this.side; }
+            set { this.side = value; }
+        }
+        public float Perimitor()
+        {
+            return 4 * side;
+        }
+        public float Area()
+        {
+            return side * side;
+        }        
+    }    
+}
